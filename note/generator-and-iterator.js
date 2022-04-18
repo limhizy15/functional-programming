c437184594ecca795db7839fa9626f6e0f71ec9a
heeji <limhizy15@ajou.ac.kr>
2022-04-18 22:54:23 +0900

    generator and iterator

--- note/generator-and-iterator.js
+++ note/generator-and-iterator.js
@@ -16,3 +16,35 @@ log(iter.next());
 /**
  * odds
  */
+function *infinite(i = 0) {
+  while(true) yield i++;
+}
+
+function *limit(lim, iterator) {
+  for (const a of iterator) {
+    // log(a);
+    if(a > lim) return;
+    yield a;
+  }
+}
+
+function *odds(l) {
+  // log(JSON.stringify(infinite()));
+  // for (const b of infinite()) log(b);
+  for(const a of limit(l, infinite())) yield a;
+}
+
+let iter2 = odds(10);
+// log(iter2);
+// log(iter2.next());
+// log(iter2.next());
+// log(iter2.next());
+// log(iter2.next());
+
+for(const a of iter2) {
+  log(a);
+}
+
+/**
+ * spread
+ */
